48637b8a5015a4b369f71544fe45c706f19ea4fb
luis <lhjuca@gmail.com>
2024-06-23 23:08:53 -0300

    feat: added list all vehicles function, adjusted identation

--- js/menu.js
+++ js/menu.js
@@ -12,4 +12,27 @@ $(document).ready(
         window.location.href = 'search-vehicle.html';
     });
 
+    $('#listAllVehiclesButton').click(function (e) {
+        let vehicles = JSON.parse(localStorage.getItem('vehicles')) || [];
+        let carList = $('#carList');
+        console.log(vehicles);
+        carList.empty();
+
+        if (vehicles.length === 0) {
+            carList.append('<li>No vehicles in the database.</li>');
+        } else {
+            vehicles.forEach(vehicle => {
+                carList.append(
+                    `<li>
+                        <p>Brand: ${vehicle.brand}</p>
+                        <p>Model: ${vehicle.model}</p>
+                        <p>Color: ${vehicle.color}</p>
+                        <p>Plate: ${vehicle.plate}</p>
+                    </li>`
+                );
+            });
+        }
+    })
+
 });
+
