f6149a410291f7ef68d3f332a376c6da7604e2df
luis <lhjuca@gmail.com>
2024-06-23 23:09:47 -0300

    fix: renamed variables in addVehicle function

--- js/add-vehicle.js
+++ js/add-vehicle.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
         event.preventDefault();
 
         let vehicles = JSON.parse(localStorage.getItem('vehicles')) || [];
-        let vehicle = {
+        let addedVehicle = {
             photo: $('#carPhotoInput').val(),
-            brand: $('#carModelInput').eq(0).val(),
-            model: $('#carModelInput').eq(1).val(),
+            brand: $('#carBrandInput').val(),
+            model: $('#carModelInput').val(),
             color: $('#carColorInput').val(),
             plate: $('#carPlateInput').val()
         };
 
-        if(vehicles.some(vehicle => vehicle.plate === vehicle.plate)) {
+        if(vehicles.some(vehicle => vehicle.plate === addedVehicle.plate)) {
             alert('Vehicle alerady on database!');
             return;
         }else{
-            vehicles.push(vehicle);
+            vehicles.push(addedVehicle);
             localStorage.setItem('vehicles', JSON.stringify(vehicles));
         }        
     });
